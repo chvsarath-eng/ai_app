e9ee25d7e16d8e14303bbb44ffecc3e28a289944
chennavc <chennavc@clarkson.edu>
2026-01-31 20:47:58 -0500

    Add Google Analytics 4 tracking (G-Q12Z62SK1Q)

--- web/src/app/layout.tsx
+++ web/src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
+import { GoogleAnalytics } from '@next/third-parties/google'
 import './globals.css'
 import { Providers } from '@/app/providers'
 import { SiteHeader } from '@/components/site-header'
@@ -164,6 +165,7 @@ export default function RootLayout({
           <Footer />
         </Providers>
       </body>
+      <GoogleAnalytics gaId="G-Q12Z62SK1Q" />
     </html>
   )
 }
